0a42fc403ce1a9a916676a43004e7ab4b94181f0
TheIndra55 <theindra@protonmail.com>
2025-06-06 01:42:35 +0200

    Fix font and add dummy map

--- src/world.c
+++ src/world.c
@@ -6,8 +6,8 @@ extern char map, map_end;
 
 void World_Init(void)
 {
-    // Copy tiles to VRAM
-    bgInitTileSet(1, &patterns, &palette, 0, (&patterns_end - &patterns), 16 * 2, BG_16COLORS, 0x4000);
+    // Copy tiles to VRAMm
+    bgInitTileSet(1, &patterns, &palette, 2, (&patterns_end - &patterns), 16 * 2, BG_16COLORS, 0x4000);
 
     // Copy Map to VRAM
     bgInitMapSet(1, &map, (&map_end - &map), SC_64x64, 0x1000);
